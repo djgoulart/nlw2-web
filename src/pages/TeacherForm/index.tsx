ac64125af67ee39879e9220c0d0969aba75badad
Diego Goulart <diegogoulart.aws@gmail.com>
2020-08-08 14:10:32 -0300

    add code standards

--- src/pages/TeacherForm/index.tsx
+++ src/pages/TeacherForm/index.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import PageHeader from '../../components/PageHeader';
 
-const TeacherForm: React.FC = () => {
-  return (
-    <div id="page-teacher-form" className="container">
-      <PageHeader title="Que incrível que você quer dar aulas" />
-    </div>
-  );
-}
+const TeacherForm: React.FC = () => (
+  <div id="page-teacher-form" className="container">
+    <PageHeader title="Que incrível que você quer dar aulas" />
+  </div>
+);
 
 export default TeacherForm;
